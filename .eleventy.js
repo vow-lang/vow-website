2df67dc5c25843c7a3f0ab9cf60a23d4b2e7fd89
Paulo Matos <p@ocmatos.com>
2026-04-13 15:55:51 +0200

    Initial website: single-page 11ty site with GitHub Pages deployment
    Dark-themed landing page with feature overview, code example, and link
    to the main repo. Deployed via GitHub Actions to www.vow-lang.com.

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+    },
+  };
+};
